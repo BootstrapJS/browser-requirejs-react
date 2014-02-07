8b0e145a626732f4fda18b715426236aaacb411b
Jakob Westhoff <jakob@westhoffswelt.de>
2014-02-07 12:08:05 +0100

    Added jsx loader and JSXTransformer require configuration

--- src/require.config.js
+++ src/require.config.js
@@ -3,7 +3,9 @@ require.config({
     "basePath": './',
 
     "paths": {
-
+        "JSXTransformer": "../vendor/require-jsx/JSXTransformer",
+        "jsx": "../vendor/require-jsx/jsx",
+        "react": "../bower_components/react/react"
     },
     "shims": {
 
