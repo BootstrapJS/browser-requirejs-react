023c2212f439fc463d22382df6a4b0c201cd8ba2
Jakob Westhoff <jakob@westhoffswelt.de>
2014-08-22 13:58:04 +0200

    Added creation of "styled" element

--- Library/Components/HelloMessage.jsx
+++ Library/Components/HelloMessage.jsx
@@ -10,6 +10,9 @@ define([
                 <div>
                     <h1>Hello {this.props.name}!</h1>
                     <h2>Isn't this a beautiful day?</h2>
+                    <div className="some"><div className="less"><div className="rules">
+                        Styled by less file ;)
+                    </div></div></div>
                     <p>
                         By the way, you just executed a dynamically build <em>React
                         Application</em> using requirejs, jsx and everything. <b>Yeah!!</b>
